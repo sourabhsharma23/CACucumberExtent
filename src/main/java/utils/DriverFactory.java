8ad4c02b940b90f73350ee40828d6c7f10220916
Sourabh Sharma <sourabhs2301@gmail.com>
2025-04-18 01:11:38 +0530

    Latest code headless

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -11,11 +12,20 @@ public class DriverFactory {
     
     public static WebDriver getDriver() {
         if (driver == null) {
+        	ChromeOptions options = new ChromeOptions();
+        	 // Detect if running on Jenkins and add headless
+            if (System.getenv("JENKINS_HOME") != null) {
+                options.addArguments("--headless=new"); // or "--headless" for older Chrome
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+                System.out.println("Running in Jenkins - launching Chrome in headless mode.");
+            }
             driver = new ChromeDriver();  // or use WebDriverManager for setup
         }
         return driver;
     }
-
+    
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
